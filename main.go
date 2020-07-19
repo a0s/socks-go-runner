e907ffd3d6d913b36f2e9dae7399342aedbf3470
Anton Osenenko <418868+a0s@users.noreply.github.com>
2020-07-19 17:46:27 +0300

    refactoring

--- main.go
+++ main.go
@@ -17,22 +17,35 @@ var (
 	socks5Enabled = flag.Bool("socks5", false, "enable socks5")
 )
 
-func main() {
-	flag.Parse()
+func buildAddressString(host string, port uint64) string {
+	return fmt.Sprintf("%v:%v", host, port)
+}
 
+func buildStatusString(address string, socks4Enabled bool, socks5Enabled bool) string {
 	var protocols []string
-	if *socks4Enabled == true {
+
+	if socks4Enabled == true {
 		protocols = append(protocols, "socks4")
 	}
-	if *socks5Enabled == true {
+	if socks5Enabled == true {
 		protocols = append(protocols, "socks5")
 	}
 	if len(protocols) == 0 {
 		protocols = append(protocols, "none")
 	}
+
 	withProtocols := strings.Join(protocols, ",")
-	address := fmt.Sprintf("%v:%v", *host, *port)
-	log.Printf("bind to %v with %v", address, withProtocols)
+	statusString := fmt.Sprintf("bind to %v with %v", address, withProtocols)
+
+	return statusString
+}
+
+func main() {
+	flag.Parse()
+
+	address := buildAddressString(*host, *port)
+	statusString := buildStatusString(address, *socks4Enabled, *socks5Enabled)
+	log.Printf(statusString)
 
 	conn, err := net.Listen("tcp", address)
 	if err != nil {
